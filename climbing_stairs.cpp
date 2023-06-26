170bbef119b7bcaf1d1577912931c5cf5b4cb758
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-06-26 23:34:23 +0530

    Create climbing_stairs.cpp
    Time complexity: o(n)
    Space complexity: o(n), this code uses dp of size n+1 that's why it cover the space of o(n)

---
+++ climbing_stairs.cpp
@@ -0,0 +1,29 @@
+///for n=2 : (1+1, 2)
+///fpr n=3 : (1+1+1, 2+1, 1+2)
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int fun(int n)
+{
+    vector<int>dp(n+1,-1);
+    dp[0]=1;
+    dp[1]=1;
+    
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    cout<<fun(n);
+    }
+}
